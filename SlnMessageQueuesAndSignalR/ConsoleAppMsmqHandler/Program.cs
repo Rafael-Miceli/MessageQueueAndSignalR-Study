4f3517684ad09c0f730ad603d224fcc875bccade
Rafael Miceli <rafael.miceli@hotmail.com>
2014-08-20 15:20:07 -0300

    Up project of study

---
+++ SlnMessageQueuesAndSignalR/ConsoleAppMsmqHandler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleAppMsmqHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var queue = new MessageQueue(@".\private$\TestQueue"))
+            {
+                while (true)
+                {
+                    Console.WriteLine("Listening TestQueue");
+
+                    var message = queue.Receive();
+
+                    var bodyReader = new StreamReader(message.BodyStream);
+
+                    var xmlBody = bodyReader.ReadToEnd();
+
+                    Console.WriteLine("Message Received: {0}", xmlBody);
+                }
+            }
+        }
+    }
+}
---
+++ SlnMessageQueuesAndSignalR/ConsoleAppMsmqStudy/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ConsoleAppMsmqStudy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing Message Queue Tester");
+
+            using (var queue = new MessageQueue(@".\private$\TestQueue"))
+            {
+
+                queue.Send("Message: Hello World 2");
+
+            }
+
+            Console.WriteLine("First Message sended");
+
+            var newMessage = new NewMessage
+            {
+                Message = "Mensagem de teste 2"
+            };
+
+            if (MessageQueue.Exists(@".\private$\TestTransactionalQueue"))
+            {
+                var queue = new MessageQueue(@".\private$\TestTransactionalQueue", true);
+
+
+                using (queue)
+                {
+                    var tx = new MessageQueueTransaction();
+
+                    var message = new Message();
+
+                    var xmlSerializer = new XmlSerializer(typeof (NewMessage));
+
+                    var newMessageStream = new MemoryStream();
+
+                    xmlSerializer.Serialize(newMessageStream, newMessage);
+
+                    message.BodyStream = newMessageStream;
+
+                    tx.Begin();
+                    queue.Send(message, tx);
+                    tx.Commit();
+                }
+            }
+            Console.WriteLine("Second Message sended with transaction");
+        }
+    }
+
+    public class NewMessage
+    {
+        public string Message { get; set; }
+    }
+}
